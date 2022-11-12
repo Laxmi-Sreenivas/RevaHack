8e3fc016969183e9a2f78ca693d066090a4966ec
Laxmi-Sreenivas <lakshmisreenivasmail@gmail.com>
2022-11-13 01:30:14 +0530

    Report Page Done

--- swasthya/lib/reportspage/report.dart
+++ swasthya/lib/reportspage/report.dart
@@ -1,9 +1,8 @@
 import 'package:swasthya/TopBar/topbar.dart';
 import 'package:swasthya/navigationpage/navigationbar.dart';
 import 'package:swasthya/reportspage/reportcard.dart';
-
-
 import 'package:flutter/material.dart';
+import 'package:swasthya/services/services.dart';
 
 class Report extends StatefulWidget {
   const Report({Key? key}) : super(key: key);
@@ -15,25 +14,44 @@ class Report extends StatefulWidget {
 }
 
 class ReportState extends State<Report> {
+  AuthService _auth = AuthService();
+  List<ReportCard> reportList = [];
+  bool isLoading = true;
+
+  void getReports() async {
+    List<List<String>> reportRaw = await _auth.getHealthrec();
+    reportRaw.forEach((element) {
+      reportList.add(ReportCard(
+        reportData: element,
+      ));
+    });
+    isLoading = false;
+    if (mounted) setState(() {});
+  }
+
+  void initState(){
+    super.initState();
+    getReports();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: TopBar(),
-      body: SingleChildScrollView(child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.only(left: 25,top: 15,bottom: 10),
-            child: Text("Your Reports",style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold)),),
-          
-          ReportCard(),
-          ReportCard(),
-          ReportCard(),
-          ReportCard(),
-          ReportCard(),
-        ],
-      ),),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.only(left: 25, top: 15, bottom: 10),
+              child: Text("Your Reports",
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+            ),
+            ...reportList,
+          ],
+        ),
+      ),
       bottomNavigationBar: NavBar(
         pageIndex: 1,
       ),
